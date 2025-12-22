072c375405a23e4351a3f06d7a4a452f34c27335
Facundo Brun <facundobrun0303@gmail.com>
2025-12-22 12:31:46 -0300

    half stripe

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -20,6 +21,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', fn () => auth()->check() ? redirect('home') : view('welcome'));
 
 Auth::routes();
+
+Route::get('/billing-portal', function (Request $request) {
+    return $request->user()->redirectToBillingPortal();
+});
+
+Route::get('/checkout', function (Request $request) {
+    return $request->user()->newSubscription(
+        'default', config('stripe.price_id')
+    )->checkout();
+
+    // ...
+});
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
